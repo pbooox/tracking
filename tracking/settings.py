02fcf7dafa20b029c3062e993e1f6565e36dae73
Pedro <pboox.y@gmail.com>
2020-02-14 11:34:14 -0600

    cambio de db a mysql

--- tracking/settings.py
+++ tracking/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'tracking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tracking',
+        'USER': 'root',
+        'PASSWORD': 'pedro1234',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
